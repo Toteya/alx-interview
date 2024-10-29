a425b1d99d4a071fdd050bbee4cff869d43b1911
Toteya <111172685+Toteya@users.noreply.github.com>
2024-10-29 20:20:55 +0200

    improvement on code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,16 +8,16 @@ def validUTF8(data):
     """ Checks if the given data is a valid UTF-8 encoding
     """
     for num in data:
-        if num < 0xFF and 0x80 & num == 0:
+        if num >> 7 == 0:
             continue
-        if num <= 0xCF and 0x20 ^ num >= 0xE0:
-            if (num >> 6) | 0x02 == 2:
+        if num <= 0xDF and 0x20 ^ num >= 0xE0:
+            if ((num & 0x0F) >> 6) | 0x02 == 2:
                 continue
         if num <= 0xEF and 0x10 ^ num >= 0xF0:
-            if (num >> 6) | 0x02 == 2:
+            if ((num & 0x0F) >> 6) | 0x02 == 2:
                 continue
         if num < 0xF7 and 0x08 ^ num >= 0xF8:
-            if (num >> 6) | 0x02 == 2:
+            if ((num & 0x0F) >> 6) | 0x02 == 2:
                 continue
         return False
     return True
