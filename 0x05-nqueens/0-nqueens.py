82e53b8ad7d88aa39f096c5ab08a116bb1f0f9ac
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-06 17:47:39 +0200

    solution; readme

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""
+0-nqueens:
+Finds all possible solutions to N-Queens problem
+"""
+import sys
+
+
+class Queen:
+    """ A queen chess piece
+    """
+    file: int
+    rank: int
+
+    def __init__(self, file: int, rank: int):
+        """Initialise the Queen instance"""
+        self.rank = rank
+        self.file = file
+
+    def get_pos(self):
+        """Returns the position of the Queen instance as a list [file, rank]"""
+        return [self.file, self.rank]
+
+    def __repr__(self) -> str:
+        """Returns a print-friendly representation of the Queen instance"""
+        return 'Queen[{}][{}]'.format(self.file, self.rank)
+
+
+def isAttacking(q1: Queen, q2: Queen) -> bool:
+    """ Returns true if the two given Queens are attacking eachother
+    """
+    if any([sameFile(q1, q2), sameRank(q1, q2), sameDiagonalPath(q1, q2)]):
+        return True
+    return False
+
+
+def sameFile(q1: Queen, q2: Queen) -> bool:
+    """ Returns true if the two Queens have the same file and therefore are
+    attacking eachother
+    """
+    if q1.file == q2.file:
+        return True
+    return False
+
+
+def sameRank(q1: Queen, q2: Queen) -> bool:
+    """ Returns true if the two Queens have the same rank and therefore are
+    attacking eachother
+    """
+    if q1.rank == q2.rank:
+        return True
+    return False
+
+
+def sameDiagonalPath(q1: Queen, q2: Queen) -> bool:
+    """ Check if the two Queens are on the same diagonal path and therefore
+    are attacking eachother
+    """
+    if abs(q1.file - q2.file) == abs(q1.rank - q2.rank):
+        return True
+    return False
+
+
+def main():
+    N: int
+
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if N < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    queens = []
+    for rank in range(N):
+        row = []
+        for file in range(N):
+            row.append(Queen(file, rank))
+        queens.append(row)
+
+    solutions = []
+    for rank in range(N):
+        for file in range(N):
+            s = findSolution(N, file, rank, queens)
+            if s not in solutions and len(s) == N:
+                solutions.append(s)
+
+    solutions_list = [[queen.get_pos() for queen in s] for s in solutions]
+
+    for s in solutions_list:
+        print(s)
+
+
+def findSolution(N: int, file: int, rank: int, queens: list) -> set:
+    """ Finds and return a solution if the first queen is placed on the given
+    position
+    """
+    solution = {queens[rank][file]}
+    for rank in range(N):
+        for file in range(N):
+            new_queen = queens[rank][file]
+            if any(isAttacking(new_queen, queen) for queen in solution):
+                continue
+            solution.add(new_queen)
+
+    return solution
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,23 @@
+# 0x05. N Queens
+
+## Concepts and Resources:
+[Mocl Interview](https://intranet.alxswe.com/rltoken/aQ3uJmGVeZa-R6B1jYTjXg)
+
+Backtracking Algorithms:
+* Understanding how backtracking algorithms work to explore all potential solutions to a problem and backtrack when a solution cannot be completed.
+* [Backtracking Introduction](https://intranet.alxswe.com/rltoken/Gbaz9HkwvR9FX4zjBt9dSw)
+
+Recursion:
+* Using recursive functions to implement backtracking algorithms.
+* [Recursion in Python](https://intranet.alxswe.com/rltoken/X1vaNXgy_pPyvKfOJm90XQ)
+
+List Manipulations in Python:
+* Creating and manipulating lists, especially to store the positions of queens on the board.
+* [Python Lists](https://intranet.alxswe.com/rltoken/P3KbYxmdtSeoJvVfr9Iv0w)
+
+Python Command Line Arguments:
+* Handling command-line arguments with the sys module.
+* [Command Line Arguments in Python](https://intranet.alxswe.com/rltoken/2IF4V6xsY_Nq-xcGDK3Bhw)
+
+## Tasks:
+* Mandatory: 0
