0b96cbf7e8d172fff91669dfe3b6f0d09203df41
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-12 19:26:24 +0200

    custom promise wrapper for request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,28 +3,41 @@
 */
 
 const request = require('request');
-const { promisify } = require('util');
-const requestPromise = promisify(request);
 
 const filmId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
 
+function makePromise(func) {
+  return (...args) => {
+    return new Promise((resolve, reject) => {
+      func(...args, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({response, body});
+        }
+      })
+    })
+  }
+}
+
+const promiseRequest = makePromise(request);
+
 async function getCharacter (url) {
-  const response = await requestPromise(url);
+  const {response, body} = await promiseRequest(url);
   if (response.statusCode === 200) {
-    const character = JSON.parse(response.body);
+    const character = JSON.parse(body);
     const charName = character.name;
     console.log(charName);
   }
 }
 
 async function getFilmCharacters (url) {
-  const response = await requestPromise(url);
+  const {response, body} = await promiseRequest(url);
   if (response.statusCode === 200) {
-    const results = JSON.parse(response.body);
+    const results = JSON.parse(body);
     const characters = results.characters;
     for (const characterUrl of characters) {
-      // console.log(characterUrl)
       await getCharacter(characterUrl);
     }
   }
