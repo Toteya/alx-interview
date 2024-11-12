b362340595240ef714debdd8b982d3e684140fe1
Toteya <111172685+Toteya@users.noreply.github.com>
2024-11-12 19:29:35 +0200

    fixed semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,24 +7,24 @@ const request = require('request');
 const filmId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
 
-function makePromise(func) {
+function makePromise (func) {
   return (...args) => {
     return new Promise((resolve, reject) => {
       func(...args, (error, response, body) => {
         if (error) {
           reject(error);
         } else {
-          resolve({response, body});
+          resolve({ response, body });
         }
-      })
-    })
-  }
+      });
+    });
+  };
 }
 
 const promiseRequest = makePromise(request);
 
 async function getCharacter (url) {
-  const {response, body} = await promiseRequest(url);
+  const { response, body } = await promiseRequest(url);
   if (response.statusCode === 200) {
     const character = JSON.parse(body);
     const charName = character.name;
@@ -33,7 +33,7 @@ async function getCharacter (url) {
 }
 
 async function getFilmCharacters (url) {
-  const {response, body} = await promiseRequest(url);
+  const { response, body } = await promiseRequest(url);
   if (response.statusCode === 200) {
     const results = JSON.parse(body);
     const characters = results.characters;
