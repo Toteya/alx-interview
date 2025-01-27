a19115ed3d0c0ffef9f4585fbd14a50f7d79931b
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-27 13:36:20 +0200

    pycodestyle

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,21 +3,22 @@
 module 0-making_change
 """
 
+
 def makeChange(coins, total):
     """ Returns the fewest number of coins needed to meet the given total
     """
     if total <= 0:
         return 0
-    
+
     coin_count = 0
-    
+
     while len(coins) > 0:
         max_coin = max(coins)
         coins.remove(max_coin)
 
         if (total < max_coin):
             continue
-        
+
         coin_count += int(total / max_coin)
         total = total % max_coin
 
@@ -27,4 +28,4 @@ def makeChange(coins, total):
     if total > 0:
         return -1
     else:
-        return  coin_count
+        return coin_count
